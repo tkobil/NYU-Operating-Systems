cd04b86075605fe08ed2d6b821b2e1fb5474ffaf
Tyler Kobil <tylerkobil@Tylers-Air.fios-router.home>
2021-09-26 16:07:07 -0400

    cli

--- hw1/uniq.c
+++ hw1/uniq.c
@@ -18,39 +18,70 @@ char last_line[6];
 char this_line[6];
 
 void usage(int fd_out) {
-    printf(fd_out, "usage: uniq input-file-name");
+    printf(fd_out, "usage: uniq input-file-name -w [OPTIONAL] -i [OPTIONAL] -c [OPTIONAL]");
+}
+
+void to_upper(char *str, char *copy_str) {
+    int i;
+    for (i = 0; str[i]!='\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            copy_str[i] = str[i] -32;
+        } else {
+            copy_str[i] = str[i];
+        }
+    }
 }
 
 void uniq(int fd_in, int count, int duplicate, int ignore) {
-    
-    //while read in line exists
+    int dupe_count = 1;
     int infile_num_bytes, n;
     if ((n = read(fd_in, last_line, sizeof(last_line)) <= 0)) {
         printf(STD_OUT, "error - file empty");
         exit();
     }
 
-    while ((infile_num_bytes = read(fd_in, this_line, sizeof(this_line))) > 0) { // consider read() wrapper for readline()
-        // TODO - need to account for a line being longer than 512 bytes. 
+    while ((infile_num_bytes = read(fd_in, this_line, sizeof(this_line))) > 0) { 
+        // TODO - need to account for a line being longer than 512 bytes - readline() wrapper for read()
+
 
         // if dupe exists, just leave last_line as is
         // and continue. Otherwise, print last line, 
         // set last_line to this line, and continue.
-        if (strcmp(last_line, this_line) != 0) {
+        char last_line_cmp[strlen(last_line)];
+        char this_line_cmp[strlen(this_line)];
+        if (ignore) {
+            to_upper(last_line, last_line_cmp);
+            to_upper(this_line, this_line_cmp);
+        } else {
+            strcpy(last_line_cmp, last_line);
+            strcpy(this_line_cmp, this_line);
+        }
+
+        if (strcmp(last_line_cmp, this_line_cmp) != 0) {
             // dupe not found
-            printf(STD_OUT, "%s", last_line);
+            if (count) {
+                printf(STD_OUT, "%d %s", dupe_count, last_line);
+            } else {
+                printf(STD_OUT, "%s", last_line);
+            }
             strcpy(last_line, this_line);
+            dupe_count = 1;
+        } else {
+            dupe_count++;
         }
     }
     // Always print last line. If no dupe found on last 2 lines, 
     // only the 2nd to last will have printed. Alternatively, if dupe was found,
     // the duplicated line needs to be printed here.
-    printf(STD_OUT, "%s", last_line);
+    if (count) {
+        printf(STD_OUT, "%d %s", dupe_count, last_line);
+    } else {
+        printf(STD_OUT, "%s", last_line);
+    }
 }
 
 int main(int argc, char *argv[]) {
     int in_fd;
-    // Flag definitions TODO - consider using bitwise logic to save on memory...
     int count, duplicate, ignore;
     count = duplicate = ignore = 0;
 
@@ -59,12 +90,33 @@ int main(int argc, char *argv[]) {
         exit();
     }
     else {
-        // argv[0] is uniq
-        // argv[1] is input file name
+        // argv[0] is uniq, argv[1] is input file name
         in_fd = open(argv[1], O_RDONLY);
-        // TODO - implement -c, -d, -i flags with case statement block
-    }
 
+        if (argc > 2) {
+            for (int i = 2; i < argc; i++) {
+                if (strlen(argv[i]) != 2) {
+                    usage(STD_OUT);
+                    exit();
+                }
+                switch (argv[i][1]) {
+                    case 'd' :
+                        duplicate = 1;
+                        break;
+                    case 'c' :
+                        count = 1;
+                        break;
+                    case 'i' :
+                        ignore = 1;
+                        break;
+                    default:
+                        usage(STD_OUT);
+                        exit();
+                }
+
+            }
+        }
+    }
     uniq(in_fd, count, duplicate, ignore);
     exit();
     return 0;
