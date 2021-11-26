76ee9475ebba9ce98e1e923cd0dbda190f0875a7
Tyler Kobil <tkobil17@gmail.com>
2021-11-26 15:22:40 -0500

    data gathering

--- final_project/find_file_size.py
+++ final_project/find_file_size.py
@@ -6,14 +6,9 @@ import pandas
 
 def create_file_size(size, name):
     # Need to write in chunks for very large numbers!
-    with open(name, 'a') as fd:
-        write_size = 100000
-        num_writes = int(size/write_size)
-        for i in range(0, num_writes):
-            if i == num_writes - 1:
-                write_size+= size%write_size
-            
-            fd.write('i'*write_size)
+    write_block_default_size = 100000
+    num_writes = int(size/write_block_default_size)
+    os.system(f"./part_one {name} -w {write_block_default_size} {num_writes}")
 
 def time_run(name, block_size, block_count, mode):
     t1 = time.time()
@@ -24,25 +19,26 @@ def time_run(name, block_size, block_count, mode):
 
 
 def main():
-    test_cases = "test_cases.json"
+    test_cases = "test_cases_part_four.json"
     with open(test_cases, 'r') as test_cases:
         runs = json.loads(test_cases.read())
 
 
     for run in runs:
         if run['mode'] == 'r':
-            if os.path.exists(run['size']):
+            if os.path.exists(run['name']):
                 os.remove(run['name'])
             create_file_size(run['size'], run['name'])
 
-        run['time'] = time_run(run['name'], run['block_size'], run['block_count'], run['mode'])
+        run['time_no_cache'] = time_run(run['name'], run['block_size'], run['block_count'], run['mode'])
+        run['time_cache'] = time_run(run['name'], run['block_size'], run['block_count'], run['mode'])
 
         if run['mode'] == 'r':
             os.remove(run['name'])
 
     df = pandas.DataFrame(runs)
     print(df)
-    df.to_csv('part_two_analysis.csv')
+    df.to_csv('part_four_analysis.csv')
 
 if __name__ == "__main__":
     main()
