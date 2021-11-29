4f9223776f21427f45e17e46a4307865339baa7f
Tyler Kobil <tkobil17@gmail.com>
2021-11-29 16:07:20 -0500

    updating

--- final_project/run.c
+++ final_project/run.c
@@ -14,6 +14,8 @@ typedef struct {
     unsigned int *buf;
     unsigned int xor;
     int finished;
+    int thread_num;
+    int num_threads;
 } thread_arg;
 
 void usage() {
@@ -31,8 +33,14 @@ unsigned int xorbuf(unsigned int *buffer, int size) {
 void *thread_read(void *arg) {
     thread_arg *t_arg = (thread_arg *)arg;
     int r;
-    while ((r = read(t_arg->fd, t_arg->buf, t_arg->block_size)) > 0) {
-        t_arg->xor ^= xorbuf(t_arg->buf, r/sizeof(unsigned int));
+    int size;
+    int offset = t_arg->thread_num * t_arg->block_size;
+    int count = 0;
+    while ((r = pread(t_arg->fd, t_arg->buf, t_arg->block_size, offset)) > 0) {
+        size = r/sizeof(unsigned int);
+        t_arg->xor ^= xorbuf(t_arg->buf, size);
+        count++;
+        offset = ((t_arg->num_threads * count) + t_arg->thread_num) * t_arg->block_size;
     }
     // int r = read(t_arg->fd, t_arg->buf, t_arg->block_size);
     t_arg->finished = TRUE;
@@ -41,25 +49,26 @@ void *thread_read(void *arg) {
 
 void disk_read(char *filename, int block_size, int num_threads) {
     int fd = open(filename, O_RDONLY);
-    thread_arg *thread_args[num_threads];
+    thread_arg thread_args[num_threads];
     int i;
     int xor = 0;
     pthread_t threads[num_threads];
     unsigned int buf_size = block_size / sizeof(unsigned int);
-    // Initialize buffers and thread args
+    //Initialize buffers and thread args
     for (i=0; i < num_threads; i++) {
-        unsigned int buf[buf_size];
-        thread_arg *arg;
-        arg->block_size = buf_size * sizeof(unsigned int);
-        arg->fd = fd;
-        arg->buf = buf;
-        arg->xor = 0;
-        arg->finished = FALSE;
+        thread_arg arg;
+        arg.block_size = buf_size * sizeof(unsigned int);
+        arg.fd = fd;
+        arg.thread_num = i;
+        arg.num_threads = num_threads;
+        arg.buf = (unsigned int *)malloc(buf_size * sizeof(unsigned int));
+        arg.xor = 0;
+        arg.finished = FALSE;
         thread_args[i] = arg;
     }
 
     for (i=0; i < num_threads; i++) {
-        pthread_create(&threads[i], NULL, thread_read, (void *)thread_args[i]);
+        pthread_create(&threads[i], NULL, thread_read, (void *)(&thread_args[i]));
     }
 
     // Wait till at least one thread finishes. As soon as it does,
@@ -67,7 +76,7 @@ void disk_read(char *filename, int block_size, int num_threads) {
     int loop_break = FALSE;
     while(!loop_break) {
         for (i=0; i < num_threads; i++) {
-            if (thread_args[i]->finished) {
+            if (thread_args[i].finished) {
                 loop_break = TRUE;
             }
         }
@@ -75,7 +84,12 @@ void disk_read(char *filename, int block_size, int num_threads) {
 
     for (i=0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
-        xor ^= thread_args[i]->xor;
+        printf("thread %d\n", i);
+        for (int z=0; z < sizeof(thread_args[i].buf); z++) {
+            printf("%d\n", thread_args[i].buf[z]);
+        }
+        // printf("%d\n", thread_args[i]->buf);
+        xor ^= thread_args[i].xor;
     }
 
     printf("xor: %08x\n", xor);
@@ -107,11 +121,7 @@ int main(int argc, char *argv[]) {
 
     // TODO - Are we able to assume 
     // CLI args will come in order? @ Prof
-
-    // pull filename
-
     filename = argv[1];
-
 
     // get read/write flag
     if (strlen(argv[2]) != 2) {
